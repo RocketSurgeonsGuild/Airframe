3b81d922402bef0f9b8a4335944ef5cfcf7dca40
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-08-18 18:06:47 -0500

    Added Xamarin.Forms application base class

---
+++ src/Forms/ApplicationBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Rocket.Surgery.ReactiveUI.Forms
+{
+    /// <summary>
+    /// Base application abstraction.
+    /// </summary>
+    /// <seealso cref="Xamarin.Forms.Application" />
+    [SuppressMessage("Microsoft.Usage", "CA2214", Justification = "Justification", MessageId = "MessageId", Scope = "Scope", Target = "Target")]
+    public abstract class ApplicationBase : Application
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationBase"/> class.
+        /// </summary>
+        protected ApplicationBase()
+        {
+            ComposeApplicationRoot();
+        }
+
+        /// <summary>
+        /// Gets the current application.
+        /// </summary>
+        public static new ApplicationBase Current => Application.Current as ApplicationBase;
+
+        /// <summary>
+        /// Composes the applications composition root.
+        /// </summary>
+        protected abstract void ComposeApplicationRoot();
+    }
+}
