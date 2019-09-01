98f1be03881a3bed63d407327106839471b50c00
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2019-08-31 23:34:36 -0500

    Added NSUserNotifications

---
+++ src/Controls/Platforms/appkit/UserNotificationExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reactive.Linq;
+using Foundation;
+using static AppKit.NSApplication;
+
+namespace Rocket.Surgery.ReactiveUI
+{
+    /// <summary>
+    /// Observable extensions for user notification.
+    /// </summary>
+    public static class UserNotificationExtensions
+    {
+        private static readonly NSUserNotificationCenter UserNotificationCenter;
+
+        static UserNotificationExtensions()
+        {
+            UserNotificationCenter = NSUserNotificationCenter.DefaultUserNotificationCenter;
+
+            // Make sure the notification fires even if the app is TopMost
+            NSUserNotificationCenter.DefaultUserNotificationCenter.ShouldPresentNotification = (c, n) => true;
+        }
+
+        /// <summary>
+        /// Notifies the user.
+        /// </summary>
+        /// <typeparam name="T">The source type.</typeparam>
+        /// <param name="sender">The sender.</param>
+        /// <param name="title">The title.</param>
+        /// <param name="informativeText">The informative text.</param>
+        /// <returns>The source observable.</returns>
+        public static IObservable<T> NotifyUser<T>(this IObservable<T> sender, string title, string informativeText)
+        {
+            var notification = new NSUserNotification();
+            notification.Title = title;
+            notification.InformativeText = informativeText;
+            notification.DeliveryDate = (NSDate)DateTime.Now;
+            notification.SoundName = NSUserNotification.NSUserNotificationDefaultSoundName;
+            notification.HasActionButton = true;
+
+            UserNotificationCenter.ScheduleNotification(notification);
+            return sender;
+        }
+
+        /// <summary>
+        /// Notifies the user.
+        /// </summary>
+        /// <typeparam name="T">The source type.</typeparam>
+        /// <param name="sender">The sender.</param>
+        /// <param name="factory">The factory.</param>
+        /// <returns>The source observable.</returns>
+        public static IObservable<T> NotifyUser<T>(this IObservable<T> sender, Func<NSUserNotification> factory)
+        {
+            UserNotificationCenter.ScheduleNotification(factory());
+            return sender;
+        }
+    }
+}
