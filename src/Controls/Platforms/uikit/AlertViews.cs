6606d14b67d7fc4430cab2f83f4fcfd2696e1eed
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2019-09-14 15:54:35 -0500

    Added appearance overloads to AlertViews

--- src/Controls/Platforms/uikit/AlertViews.cs
+++ src/Controls/Platforms/uikit/AlertViews.cs
@@ -1,3 +1,4 @@
+using System;
 using GlobalToast;
 using UIKit;
 
@@ -6,7 +7,7 @@ namespace Rocket.Surgery.ReactiveUI
     /// <summary>
     /// Alerts.
     /// </summary>
-    public class AlertViews
+    public static class AlertViews
     {
         /// <summary>
         /// Displays a toast message.
@@ -21,6 +22,46 @@ namespace Rocket.Surgery.ReactiveUI
             Toast
                 .MakeToast(message)
                 .SetTitle(title)
+                .SetPosition(position)
+                .SetDuration(duration)
+                .SetAutoDismiss(true)
+                .Show();
+        }
+
+        /// <summary>
+        /// Displays a toast message.
+        /// </summary>
+        /// <param name="title">The title for the toast message.</param>
+        /// <param name="message">The message to display in the toast.</param>
+        /// <param name="appearance">The appearance.</param>
+        /// <param name="duration">The duration to display the toast.</param>
+        /// <param name="position">The position for the toast to display.</param>
+        public static void ShowToast(string title, string message, ToastAppearance appearance, double duration = 2.0, ToastPosition position = ToastPosition.Center)
+        {
+            Toast
+                .MakeToast(message)
+                .SetTitle(title)
+                .SetAppearance(appearance)
+                .SetPosition(position)
+                .SetDuration(duration)
+                .SetAutoDismiss(true)
+                .Show();
+        }
+
+        /// <summary>
+        /// Displays a toast message.
+        /// </summary>
+        /// <param name="title">The title for the toast message.</param>
+        /// <param name="message">The message to display in the toast.</param>
+        /// <param name="appearance">The appearance.</param>
+        /// <param name="duration">The duration to display the toast.</param>
+        /// <param name="position">The position for the toast to display.</param>
+        public static void ShowToast(string title, string message, Func<ToastAppearance> appearance, double duration = 2.0, ToastPosition position = ToastPosition.Center)
+        {
+            Toast
+                .MakeToast(message)
+                .SetTitle(title)
+                .SetAppearance(appearance())
                 .SetPosition(position)
                 .SetDuration(duration)
                 .SetAutoDismiss(true)
