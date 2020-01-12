bcd2b8f9ec1eebf08aa448e647dcab63124c0b58
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-01-12 00:04:34 -0600

    fix: Remove FromEvent items from ReactiveListView

--- src/Forms/Views/ReactiveListView.cs
+++ src/Forms/Views/ReactiveListView.cs
@@ -50,71 +50,8 @@ namespace Rocket.Surgery.Airframe.Forms
         /// Initializes a new instance of the <see cref="ReactiveListView"/> class.
         /// </summary>
         public ReactiveListView()
+            : this(ListViewCachingStrategy.RecycleElement)
         {
         }
-
-        /// <summary>
-        /// Gets the an observable sequence of item appearing events.
-        /// </summary>
-        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
-        /// <returns>An observable sequence of items appearing.</returns>
-        /// <value>The ListView item appearing.</value>
-        public IObservable<TViewModel> ListViewItemAppearing<TViewModel>()
-            where TViewModel : class =>
-                Observable
-                    .FromEvent<EventHandler<ItemVisibilityEventArgs>, ItemVisibilityEventArgs>(x => ItemDisappearing += x, x => ItemDisappearing -= x)
-                    .Select(x => x.Item as TViewModel);
-
-        /// <summary>
-        /// Gets the an observable sequence of item disappearing events.
-        /// </summary>
-        /// <typeparam name="TViewModel">The type of the view model.</typeparam>
-        /// <returns>An observable sequence of items disappearing.</returns>
-        /// <value>The ListView item appearing.</value>
-        public IObservable<TViewModel> ListViewItemDisappearing<TViewModel>()
-            where TViewModel : class =>
-                Observable
-                    .FromEvent<EventHandler<ItemVisibilityEventArgs>, ItemVisibilityEventArgs>(x => ItemAppearing += x, x => ItemAppearing -= x)
-                    .Select(x => x.Item as TViewModel);
-
-        /// <summary>
-        /// Gets an observable sequence of the item tapped from the <see cref="ListView"/>.
-        /// </summary>
-        /// <typeparam name="T">The view model type.</typeparam>
-        /// <returns>An observable sequence of items selected.</returns>
-        public IObservable<T> ListViewItemTapped<T>()
-            where T : class =>
-                Observable
-                    .FromEvent<EventHandler<ItemTappedEventArgs>, ItemTappedEventArgs>(x => ItemTapped += x, x => ItemTapped -= x)
-                    .Select(args => args.Item as T);
-
-        /// <summary>
-        /// Gets an observable sequence of the item tapped from the <see cref="ListView"/>.
-        /// </summary>
-        /// <returns>An observable sequence of items selected.</returns>
-        public IObservable<object> ListViewItemTapped() =>
-            Observable
-                .FromEvent<EventHandler<ItemTappedEventArgs>, ItemTappedEventArgs>(x => ItemTapped += x, x => ItemTapped -= x)
-                .Select(args => args.Item);
-
-        /// <summary>
-        /// Gets an observable sequence of the item selected from the <see cref="ListView"/>.
-        /// </summary>
-        /// <typeparam name="T">The view model type.</typeparam>
-        /// <returns>An observable sequence of items selected.</returns>
-        public IObservable<T> ListViewItemSelected<T>()
-            where T : class =>
-                Observable
-                    .FromEvent<EventHandler<SelectedItemChangedEventArgs>, SelectedItemChangedEventArgs>(x => ItemSelected += x, x => ItemSelected -= x)
-                    .Select(args => args.SelectedItem as T);
-
-        /// <summary>
-        /// Gets an observable sequence of the item selected from the <see cref="ListView"/>.
-        /// </summary>
-        /// <returns>An observable sequence of items selected.</returns>
-        public IObservable<object> ListViewItemSelected() =>
-            Observable
-                .FromEvent<EventHandler<SelectedItemChangedEventArgs>, SelectedItemChangedEventArgs>(x => ItemSelected += x, x => ItemSelected -= x)
-                .Select(args => args.SelectedItem);
     }
 }
