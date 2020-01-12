411915a5e54fd0a76da1cfb318a33520620d7019
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-01-12 00:54:01 -0600

    feature: Added Logging Composition Module

---
+++ src/Composition/LoggingModule.cs
@@ -0,0 +1,31 @@
+using DryIoc;
+using Serilog;
+using Splat;
+
+namespace Rocket.Surgery.Airframe.Composition
+{
+    /// <summary>
+    /// Logging registrations.
+    /// </summary>
+    public class LoggingModule : DryIocModule
+    {
+        /// <inheritdoc />
+        public override void Load(IRegistrator registrar)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo
+                .AppCenterCrashes()
+                .CreateLogger();
+
+            var funcLogManager = new FuncLogManager(type =>
+            {
+                var actualLogger = global::Serilog.Log.ForContext(type);
+                return new SerilogFullLogger(actualLogger);
+            });
+
+            registrar.RegisterInstance<Serilog.ILogger>(Log.Logger);
+            registrar.Register<IFullLogger, SerilogFullLogger>(Reuse.Singleton);
+            registrar.RegisterDelegate(ctx => funcLogManager, reuse: Reuse.Singleton);
+        }
+    }
+}
