2022ba7dccc0a63d68b7a4858b4b9bba29d05dd9
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-01-15 22:41:26 -0600

    feature: Added Dry.Ioc to Xamarin.iOS ApplicationDelegateBase

--- src/Controls/Platforms/uikit/ApplicationDelegateBase.cs
+++ src/Controls/Platforms/uikit/ApplicationDelegateBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DryIoc;
 using Foundation;
+using Splat.DryIoc;
 using UIKit;
 
 namespace Rocket.Surgery.Airframe
@@ -11,6 +13,13 @@ namespace Rocket.Surgery.Airframe
     /// </summary>
     public abstract class ApplicationDelegateBase : UIApplicationDelegate
     {
+        private IContainer _container;
+
+        /// <summary>
+        /// Gets or sets the dependency inversion container.
+        /// </summary>
+        public IContainer Container { get; protected set; }
+
         /// <inheritdoc/>
         public override void FinishedLaunching(UIApplication application)
         {
@@ -30,6 +39,24 @@ namespace Rocket.Surgery.Airframe
         /// <summary>
         /// Composes the <see cref="UIApplicationDelegate"/> and registers services.
         /// </summary>
-        protected abstract void ComposeDelegate();
+        protected virtual void ComposeDelegate()
+        {
+            _container = CreateContainer();
+            RegisterServices(_container);
+            _container.UseDryIocDependencyResolver();
+            Container = _container.WithNoMoreRegistrationAllowed();
+        }
+
+        /// <summary>
+        /// Returns a new container instance.
+        /// </summary>
+        /// <returns>The container.</returns>
+        protected virtual IContainer CreateContainer() => new Container();
+
+        /// <summary>
+        /// Registers services with the <see cref="IContainer"/> instance.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        protected abstract void RegisterServices(IContainer container);
     }
 }
