c2606114e60a71aac3fb8dbc15ff971ed7e9bd4b
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-01-17 02:24:33 -0600

    fix: Remove Log.Logger instantiation

--- src/Composition/LoggingModule.cs
+++ src/Composition/LoggingModule.cs
@@ -9,34 +9,9 @@ namespace Rocket.Surgery.Airframe.Composition
     /// </summary>
     public class LoggingModule : DryIocModule
     {
-        private readonly string _appCenterSecret;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LoggingModule"/> class.
-        /// </summary>
-        public LoggingModule()
-            : this(string.Empty)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="LoggingModule"/> class.
-        /// </summary>
-        /// <param name="appCenterSecret">The app center secret.</param>
-        public LoggingModule(string appCenterSecret)
-        {
-            _appCenterSecret = appCenterSecret;
-        }
-
         /// <inheritdoc />
         public override void Load(IRegistrator registrar)
         {
-            Log.Logger =
-                new LoggerConfiguration()
-                    .WriteTo
-                    .AppCenterCrashes(_appCenterSecret)
-                    .CreateLogger();
-
             var funcLogManager = new FuncLogManager(type =>
             {
                 var actualLogger = global::Serilog.Log.ForContext(type);
