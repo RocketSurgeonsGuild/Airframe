d574994d58542bca7d7ca491a2b54303a262772d
RLittlesII <6969701+RLittlesII@users.noreply.github.com>
2020-02-13 04:01:55 -0600

    feature: Added PlatformRegistrarBase

---
+++ src/Composition/PlatformRegistrarBase.cs
@@ -0,0 +1,13 @@
+using DryIoc;
+
+namespace Rocket.Surgery.Airframe.Composition
+{
+    /// <summary>
+    /// Base platform registration object.
+    /// </summary>
+    public abstract class PlatformRegistrarBase : IPlatformRegistrar
+    {
+        /// <inheritdoc />
+        public abstract void RegisterPlatformServices(IRegistrator registrator);
+    }
+}
