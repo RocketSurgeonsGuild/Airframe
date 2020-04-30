0529d9df8ebc456ba39e3a206dd647da6762def7
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-04-30 08:19:13 -0500

    Added CoreLocationService
    Converted All EventHandlers to Observables

---
+++ src/iOS/CoreLocationService.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using CoreLocation;
+using Foundation;
+
+namespace Rocket.Surgery.Airframe.iOS
+{
+    public class CoreLocationService
+    {
+        private readonly Lazy<CLLocationManager> _locationManager = new Lazy<CLLocationManager>();
+
+        public CoreLocationService()
+        {
+            AuthorizationChanged =
+                Observable
+                    .FromEvent<EventHandler<CLAuthorizationChangedEventArgs>, CLAuthorizationChangedEventArgs>(
+                        eventHandler =>
+                        {
+                            void Handler(object sender, CLAuthorizationChangedEventArgs args) => eventHandler(args);
+                            return Handler;
+                        },
+                        x => _locationManager.Value.AuthorizationChanged += x,
+                        x => _locationManager.Value.AuthorizationChanged -= x);
+
+            LocationsUpdated =
+                Observable
+                    .FromEvent<EventHandler<CLLocationsUpdatedEventArgs>, CLLocationsUpdatedEventArgs>(
+                        x => _locationManager.Value.LocationsUpdated += x,
+                        x => _locationManager.Value.LocationsUpdated -= x);
+
+
+            LocationUpdated =
+                Observable
+                    .FromEvent<EventHandler<CLLocationUpdatedEventArgs>, CLLocationUpdatedEventArgs>(
+                        x => _locationManager.Value.UpdatedLocation += x,
+                        x => _locationManager.Value.UpdatedLocation -= x);
+
+            LocationUpdatesPaused =
+                Observable
+                    .FromEventPattern(
+                        x => _locationManager.Value.LocationUpdatesPaused += x,
+                        x => _locationManager.Value.LocationUpdatesPaused -= x);
+
+            LocationUpdatesResumed =
+                Observable
+                    .FromEventPattern(
+                        x => _locationManager.Value.LocationUpdatesResumed += x,
+                        x => _locationManager.Value.LocationUpdatesResumed -= x);
+
+            MonitoringRegion =
+                Observable
+                    .FromEvent<EventHandler<CLRegionEventArgs>, CLRegionEventArgs>(
+                        x => _locationManager.Value.DidStartMonitoringForRegion += x,
+                        x => _locationManager.Value.DidStartMonitoringForRegion -= x);
+
+            RegionStateDetermined =
+                Observable
+                    .FromEvent<EventHandler<CLRegionStateDeterminedEventArgs>, CLRegionStateDeterminedEventArgs>(
+                        x => _locationManager.Value.DidDetermineState += x,
+                        x => _locationManager.Value.DidDetermineState -= x);
+
+            DeferredUpdatesFinished =
+                Observable
+                    .FromEvent<EventHandler<NSErrorEventArgs>, NSErrorEventArgs>(
+                        x => _locationManager.Value.DeferredUpdatesFinished += x,
+                        x => _locationManager.Value.DeferredUpdatesFinished -= x);
+
+            Visited =
+                Observable
+                    .FromEvent<EventHandler<CLVisitedEventArgs>, CLVisitedEventArgs>(
+                        x => _locationManager.Value.DidVisit += x,
+                        x => _locationManager.Value.DidVisit -= x);
+
+            MonitoringFailed =
+                Observable
+                    .FromEvent<EventHandler<CLRegionErrorEventArgs>, CLRegionErrorEventArgs>(
+                        x => _locationManager.Value.MonitoringFailed += x,
+                        x => _locationManager.Value.MonitoringFailed -= x);
+        }
+
+        public IObservable<CLLocationUpdatedEventArgs> LocationUpdated { get; set; }
+
+        public IObservable<CLRegionErrorEventArgs> MonitoringFailed { get; set; }
+
+        public IObservable<CLVisitedEventArgs> Visited { get; set; }
+
+        public IObservable<NSErrorEventArgs> DeferredUpdatesFinished { get; set; }
+
+        public IObservable<CLRegionStateDeterminedEventArgs> RegionStateDetermined { get; set; }
+
+        public IObservable<CLRegionEventArgs> MonitoringRegion { get; set; }
+
+        public IObservable<EventPattern<object>> LocationUpdatesResumed { get; set; }
+
+        public IObservable<EventPattern<object>> LocationUpdatesPaused { get; set; }
+
+        public IObservable<CLLocationsUpdatedEventArgs> LocationsUpdated { get; set; }
+
+        public IObservable<CLAuthorizationChangedEventArgs> AuthorizationChanged { get; set; }
+    }
+}
