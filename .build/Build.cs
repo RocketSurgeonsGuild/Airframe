68999bcf5cba2c7d4108763f64790e2572a11452
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-06-27 14:05:12 -0500

    build: Fixed Rocket.Surgery.Nuke code coverage for msbuild projects

--- .build/Build.cs
+++ .build/Build.cs
@@ -1,33 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Nuke.Airframe;
 using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.CI.GitHubActions;
 using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MSBuild;
 using Rocket.Surgery.Nuke;
+using Rocket.Surgery.Nuke.ContinuousIntegration;
+using Rocket.Surgery.Nuke.DotNetCore;
+using Rocket.Surgery.Nuke.GithubActions;
 using Rocket.Surgery.Nuke.MsBuild;
+using Rocket.Surgery.Nuke.Xamarin;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
 
+[PublicAPI]
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
-class Airframe : MsBuild, IMsBuild
+[PackageIcon("https://raw.githubusercontent.com/RocketSurgeonsGuild/graphics/master/png/social-square-thrust-rounded.png")]
+[DotNetVerbosityMapping]
+[MSBuildVerbosityMapping]
+[NuGetVerbosityMapping]
+public partial class Solution : NukeBuild,
+                        ICanClean,
+                        ICanRestoreWithMsBuild,
+                        ICanTestWithDotNetCoreNoBuild,
+                        ICanPackWithMsBuild,
+                        ICanUpdateReadme,
+                        IHaveDataCollector,
+                        IHaveConfiguration<Configuration>,
+                        IGenerateCodeCoverageReport,
+                        IGenerateCodeCoverageSummary,
+                        IGenerateCodeCoverageBadges,
+                        ICanLint
 {
     /// <summary>
     /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
     /// </summary>
+    public static int Main() => Execute<Solution>(x => x.Default);
 
-    public static int Main() => Execute<Airframe>(x => x.Default);
+    [OptionalGitRepository]
+    public GitRepository? GitRepository { get; }
 
-    public Target Restore => _ => _.With(this, MsBuild.Restore);
+    private Target Default => _ => _
+       .DependsOn(Restore)
+       .DependsOn(Build)
+       .DependsOn(Test)
+       .DependsOn(Pack);
 
-    public Target Build => _ => _.With(this, MsBuild.Build);
+    public Target Build => _ => _.Inherit<ICanBuildWithMsBuild>(x => x.NetBuild);
 
-    public Target Test => _ => _.With(this, MsBuild.Test);
+    public Target Pack => _ => _.Inherit<ICanPackWithMsBuild>(x => x.NetPack)
+       .DependsOn(Clean);
 
-    public Target Pack => _ => _.With(this, MsBuild.Pack);
+    [ComputedGitVersion]
+    public GitVersion GitVersion { get; } = null!;
 
-    Target Default => _ => _
-        .DependsOn(Restore)
-        .DependsOn(Build)
-        .DependsOn(Test)
-        .DependsOn(Pack);
+    public Target Clean => _ => _.Inherit<ICanClean>(x => x.Clean);
+    public Target Restore => _ => _.Inherit<ICanRestoreWithMsBuild>(x => x.NetRestore);
+    public Target Test => _ => _.Inherit<ICanTestWithDotNetCoreNoBuild>(x => x.CoreTest);
+
+    public Target BuildVersion => _ => _.Inherit<IHaveBuildVersion>(x => x.BuildVersion)
+       .Before(Default)
+       .Before(Clean);
+
+    [Parameter("Configuration to build")]
+    public Configuration Configuration { get; } = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 }
---
+++ .build/ICanTestWithDotNetCoreNoBuild.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.CI.AzurePipelines;
+using Nuke.Common.CI.GitHubActions;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MSBuild;
+using Rocket.Surgery.Nuke;
+using Rocket.Surgery.Nuke.ContinuousIntegration;
+using Rocket.Surgery.Nuke.DotNetCore;
+using Rocket.Surgery.Nuke.GithubActions;
+using Rocket.Surgery.Nuke.MsBuild;
+using Rocket.Surgery.Nuke.Xamarin;
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotCover;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.FileSystemTasks;
+
+namespace Nuke.Airframe
+{
+    /// <summary>
+    /// Defines a `dotnet test` test run with code coverage via coverlet
+    /// </summary>
+    public interface ICanTestWithDotNetCoreNoBuild : IHaveCollectCoverage,
+        IHaveBuildTarget,
+        ITriggerCodeCoverageReports,
+        IComprehendTests,
+        IHaveTestArtifacts,
+        IHaveGitVersion,
+        IHaveSolution,
+        IHaveConfiguration,
+        IHaveOutputLogs,
+        ICan
+    {
+        /// <summary>
+        /// dotnet test
+        /// </summary>
+        public Target CoreTest => _ => _
+            .Description("Executes all the unit tests.")
+            .After(Build)
+            .OnlyWhenDynamic(() => TestsDirectory.GlobFiles("**/*.csproj").Count > 0)
+            .WhenSkipped(DependencyBehavior.Execute)
+            .Executes(
+                () =>
+                {
+                    EnsureCleanDirectory(TestResultsDirectory);
+                    CoverageDirectory.GlobFiles("*.cobertura.xml", "*.opencover.xml", "*.json", "*.info")
+                        .Where(x => Guid.TryParse(Path.GetFileName(x)?.Split('.')[0], out var _))
+                        .ForEach(DeleteFile);
+                })
+            .Executes(
+                () =>
+                {
+                    var runsettings = TestsDirectory / "coverlet.runsettings";
+                    if (!FileExists(runsettings))
+                    {
+                        runsettings = NukeBuild.TemporaryDirectory / "default.runsettings";
+                        if (!FileExists(runsettings))
+                        {
+                            using var tempFile = File.Open(runsettings, FileMode.CreateNew);
+                            typeof(ICanTestWithDotNetCore)
+                                    .Assembly
+                                    .GetManifestResourceStream("Rocket.Surgery.Nuke.default.runsettings")!.CopyTo(tempFile);
+                        }
+                    }
+
+                    MSBuildTasks.MSBuild(
+                        settings =>
+                            settings
+                                .SetSolutionFile(Solution)
+                                .SetConfiguration(global::Configuration.Debug)
+                                .SetDefaultLoggers(LogsDirectory / "test.build.log")
+                                .SetGitVersionEnvironment(GitVersion)
+                                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                                .SetPackageVersion(GitVersion.NuGetVersionV2));
+
+                    DotNetTasks.DotNetTest(
+                        s => s.SetProjectFile(Solution)
+                            .SetDefaultLoggers(LogsDirectory / "test.log")
+                            .SetGitVersionEnvironment(GitVersion)
+                            .EnableNoRestore()
+                            .SetLogger("trx")
+                            .SetConfiguration(global::Configuration.Debug)
+                            .SetNoBuild(true)
+                            // DeterministicSourcePaths being true breaks coverlet!
+                            .SetProperty("DeterministicSourcePaths", "false")
+                            .SetResultsDirectory(TestResultsDirectory)
+                            .When(
+                                !CollectCoverage,
+                                x => x.SetProperty((string) "CollectCoverage", "true")
+                                    .SetProperty("CoverageDirectory", CoverageDirectory)
+                            )
+                            .When(
+                                CollectCoverage,
+                                x => x
+                                    .SetProperty("CollectCoverage", "false")
+                                    .SetDataCollector("XPlat Code Coverage")
+                                    .SetSettingsFile(runsettings)
+                            )
+                    );
+
+                    // Ensure anything that has been dropped in the test results from a collector is
+                    // into the coverage directory
+                    foreach (var file in TestResultsDirectory
+                        .GlobFiles("**/*.cobertura.xml")
+                        .Where(x => Guid.TryParse(Path.GetFileName(x.Parent), out var _))
+                        .SelectMany(coverage => coverage.Parent.GlobFiles("*.*")))
+                    {
+                        var folderName = Path.GetFileName(file.Parent);
+                        var extensionPart = string.Join(".", Path.GetFileName(file).Split('.').Skip(1));
+                        CopyFile(
+                            file,
+                            CoverageDirectory / $"{folderName}.{extensionPart}",
+                            FileExistsPolicy.OverwriteIfNewer
+                        );
+                    }
+                }
+            );
+    }
+}
