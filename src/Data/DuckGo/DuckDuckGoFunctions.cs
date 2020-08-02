78951a396b926a247129d19ddfd70c4364d1e278
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-08-02 15:53:06 -0500

    migrated search to use functions

---
+++ src/Data/DuckGo/DuckDuckGoFunctions.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DynamicData;
+using Refit;
+
+namespace Data
+{
+    /// <summary>
+    /// <see cref="DuckDuckGoApi"/> functions.
+    /// </summary>
+    public static class DuckDuckGoFunctions
+    {
+        /// <summary>
+        /// Converts a <see cref="DuckDuckGoSearchResult"/> to an enumerable of <see cref="DuckDuckGoQueryResult"/>.
+        /// </summary>
+        /// <param name="searchResult">The search result.</param>
+        /// <returns>The enumeration of quest results.</returns>
+        public static IEnumerable<DuckDuckGoQueryResult> AsResult(this DuckDuckGoSearchResult searchResult) => searchResult.RelatedTopics.Where(x => !string.IsNullOrEmpty(x.FirstUrl));
+
+        /// <summary>
+        /// Caches the list of <see cref="DuckDuckGoQueryResult"/>.
+        /// </summary>
+        /// <param name="result">The result.</param>
+        /// <param name="cache">The cache.</param>
+        /// <returns>A completion notification.</returns>
+        public static async Task Cache(this Task<IEnumerable<DuckDuckGoQueryResult>> result, SourceCache<DuckDuckGoQueryResult, string> cache) => cache.AddOrUpdate(await result.ConfigureAwait(false));
+    }
+}
--- src/Data/DuckGo/DuckDuckGoService.cs
+++ src/Data/DuckGo/DuckDuckGoService.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DynamicData;
 using LanguageExt;
-using Refit;
 
 namespace Data
 {
@@ -14,6 +11,7 @@ namespace Data
     public class DuckDuckGoService : IDuckDuckGoService
     {
         private readonly IDuckDuckGoApi _duckDuckGoApi;
+
         private readonly SourceCache<DuckDuckGoQueryResult, string> _queryResults =
             new SourceCache<DuckDuckGoQueryResult, string>(x => x.FirstUrl);
 
@@ -28,28 +26,10 @@ namespace Data
             _queryResults.Connect();
 
         /// <inheritdoc/>
-        public async Task Query(string query)
-        {
-            var results = await _duckDuckGoApi.Search(query).ConfigureAwait(false);
-            foreach (var relatedTopic in results.RelatedTopics.Where(x => !string.IsNullOrEmpty(x.FirstUrl)))
-            {
-                _queryResults.AddOrUpdate(relatedTopic);
-            }
-        }
-
-        public async Task Query() =>
-            await _duckDuckGoApi.Search("")
+        public async Task Query(string query) =>
+            await _duckDuckGoApi.Search(query)
                                 .Map(x => x.AsResult())
                                 .Cache(_queryResults)
                                 .ConfigureAwait(true);
-    }
-
-    public static class DuckDuckGoFunctions
-    {
-        public static IDuckDuckGoApi DuckDuckGoApi() => RestService.For<IDuckDuckGoApi>("https://");
-
-        public static IEnumerable<DuckDuckGoQueryResult> AsResult(this DuckDuckGoSearchResult nuget) => nuget.RelatedTopics.Where(x => !string.IsNullOrEmpty(x.FirstUrl));
-
-        public static async Task Cache(this Task<IEnumerable<DuckDuckGoQueryResult>> result, SourceCache<DuckDuckGoQueryResult, string> cache) => cache.AddOrUpdate(await result.ConfigureAwait(false));
     }
 }
