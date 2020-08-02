6ad2d358c8d9c6b7c8f1d1fc83814949a13ecb23
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2020-08-01 21:03:13 -0500

    fix: Rocket.Surgery.Airframe.Data

--- src/Data/DuckGo/DuckDuckGoService.cs
+++ src/Data/DuckGo/DuckDuckGoService.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DynamicData;
+using LanguageExt;
+using Refit;
 
 namespace Data
 {
@@ -33,5 +36,20 @@ namespace Data
                 _queryResults.AddOrUpdate(relatedTopic);
             }
         }
+
+        public async Task Query() =>
+            await _duckDuckGoApi.Search("")
+                                .Map(x => x.AsResult())
+                                .Cache(_queryResults)
+                                .ConfigureAwait(true);
+    }
+
+    public static class DuckDuckGoFunctions
+    {
+        public static IDuckDuckGoApi DuckDuckGoApi() => RestService.For<IDuckDuckGoApi>("https://");
+
+        public static IEnumerable<DuckDuckGoQueryResult> AsResult(this DuckDuckGoSearchResult nuget) => nuget.RelatedTopics.Where(x => !string.IsNullOrEmpty(x.FirstUrl));
+
+        public static async Task Cache(this Task<IEnumerable<DuckDuckGoQueryResult>> result, SourceCache<DuckDuckGoQueryResult, string> cache) => cache.AddOrUpdate(await result.ConfigureAwait(false));
     }
 }
