5f63c91156860fbceacd33b6ffdfe2306297eaca
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2021-02-09 22:32:10 -0600

    feature: Added base exception handler (#43)

---
+++ src/Core/Exceptions/IExceptionHandler.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rocket.Surgery.Airframe.Exceptions
+{
+    /// <summary>
+    /// Interface representing an <see cref="Exception"/> <see cref="IObserver{T}"/>.
+    /// </summary>
+    public interface IExceptionHandler : IObserver<Exception>
+    {
+    }
+}
---
+++ src/Forms/ExceptionHandler.cs
@@ -0,0 +1,21 @@
+using System.Reactive.Concurrency;
+using JetBrains.Annotations;
+using ReactiveUI;
+using Rocket.Surgery.Airframe.Exceptions;
+
+namespace Rocket.Surgery.Airframe.Forms
+{
+    /// <summary>
+    /// Represents the default exception handler.
+    /// </summary>
+    public class ExceptionHandler : ExceptionHandlerBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExceptionHandler"/> class.
+        /// </summary>
+        public ExceptionHandler()
+            : base(RxApp.MainThreadScheduler)
+        {
+        }
+    }
+}
