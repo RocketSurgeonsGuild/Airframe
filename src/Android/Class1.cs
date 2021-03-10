47726d12d834bb7d04e4a0f561f2f59a14ad49e5
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2021-03-10 00:41:19 -0600

    feature: Added Android location services

---
+++ src/Android/Class1.cs
@@ -0,0 +1,10 @@
+﻿using Android.Locations;
+
+namespace Rocket.Surgery.Airframe.Droid
+{
+    public class Class1
+    {
+    }
+
+    public class LocationListenerActivity : ILocationListener { }
+}
