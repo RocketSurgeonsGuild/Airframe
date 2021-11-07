28a40ec007ca63cb42a2fd99bd7cb7871faebb24
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2021-11-07 17:33:50 -0600

    fixing net6 namespaces

--- src/Timers/IncrementTimer.cs
+++ src/Timers/IncrementTimer.cs
@@ -1,25 +1,26 @@
 using System;
 using JetBrains.Annotations;
 
-namespace Rocket.Surgery.Airframe.Timers;
-
-/// <summary>
-/// Represents an <see cref="IDecrement"/> <see cref="ObservableTimer"/>.
-/// </summary>
-public class IncrementTimer : ObservableTimer, IIncrement
+namespace Rocket.Surgery.Airframe.Timers
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="IncrementTimer"/> class.
+    /// Represents an <see cref="IDecrement"/> <see cref="ObservableTimer"/>.
     /// </summary>
-    /// <param name="schedulerProvider">The scheduler provider.</param>
-    public IncrementTimer([NotNull] ISchedulerProvider schedulerProvider)
-        : base(schedulerProvider)
+    public class IncrementTimer : ObservableTimer, IIncrement
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IncrementTimer"/> class.
+        /// </summary>
+        /// <param name="schedulerProvider">The scheduler provider.</param>
+        public IncrementTimer([NotNull] ISchedulerProvider schedulerProvider)
+            : base(schedulerProvider)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override TimeSpan TimeAccumulator(TimeSpan accumulated) => accumulated + TimeSpans.RefreshInterval;
+
+        /// <inheritdoc/>
+        protected override bool Elapse(TimeSpan elapsed) => false;
     }
-
-    /// <inheritdoc/>
-    protected override TimeSpan TimeAccumulator(TimeSpan accumulated) => accumulated + TimeSpans.RefreshInterval;
-
-    /// <inheritdoc/>
-    protected override bool Elapse(TimeSpan elapsed) => false;
 }
