2c6991ecde97ce25d51bac7c121f123de5c72576
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-04-23 01:51:08 -0500

    executing nuke so the yaml can generate

--- .build/AirframeBuild.CI.cs
+++ .build/AirframeBuild.CI.cs
@@ -7,7 +7,7 @@ using Rocket.Surgery.Nuke.DotNetCore;
 using Rocket.Surgery.Nuke.GithubActions;
 
 [GitHubActionsSteps("ci", GitHubActionsImage.MacOsLatest,
-    AutoGenerate = false,
+    AutoGenerate = true,
     On = new[] { GitHubActionsTrigger.Push },
     OnPushTags = new[] { "v*" },
     OnPushBranches = new[] { "master", "next", "feature/*" },
