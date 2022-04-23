34a18398d4823de8d215b8ae6275cd9ac02664dd
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-04-23 01:17:03 -0500

    chore: added husky commit msg hook

--- .build/AirframeBuild.cs
+++ .build/AirframeBuild.cs
@@ -21,6 +21,7 @@ using System.Linq;
 [DotNetVerbosityMapping]
 [MSBuildVerbosityMapping]
 [NuGetVerbosityMapping]
+[EnsureGitHooks(GitHook.CommitMsg)]
 public partial class AirframeBuild : NukeBuild,
                         ICanClean,
                         ICanRestoreWithMsBuild,
