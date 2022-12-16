60d6787aa57727762e9f043b621b8be13613ce02
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-12-16 14:21:29 -0600

    file scoped namespace

--- src/MicrosoftDependencyInjection/ServiceCollectionModuleExtensions.cs
+++ src/MicrosoftDependencyInjection/ServiceCollectionModuleExtensions.cs
@@ -1,29 +1,28 @@
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Rocket.Surgery.Airframe.Microsoft.Extensions.DependencyInjection
+namespace Rocket.Surgery.Airframe.Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for registering an <see cref="IServiceCollectionModule"/>.
+/// </summary>
+public static class ServiceCollectionModuleExtensions
 {
     /// <summary>
-    /// Extension methods for registering an <see cref="IServiceCollectionModule"/>.
+    /// Adds the provided <see cref="IServiceCollectionModule"/> to the <see cref="IServiceCollection"/>.
     /// </summary>
-    public static class ServiceCollectionModuleExtensions
-    {
-        /// <summary>
-        /// Adds the provided <see cref="IServiceCollectionModule"/> to the <see cref="IServiceCollection"/>.
-        /// </summary>
-        /// <param name="serviceCollection">The service collection.</param>
-        /// <param name="module">The module.</param>
-        /// <typeparam name="T">The type.</typeparam>
-        /// <returns>The service collection with dependencies registered.</returns>
-        public static IServiceCollection AddModule<T>(this IServiceCollection serviceCollection, T module)
-            where T : IServiceCollectionModule => module.Load(serviceCollection);
+    /// <param name="serviceCollection">The service collection.</param>
+    /// <param name="module">The module.</param>
+    /// <typeparam name="T">The type.</typeparam>
+    /// <returns>The service collection with dependencies registered.</returns>
+    public static IServiceCollection AddModule<T>(this IServiceCollection serviceCollection, T module)
+        where T : IServiceCollectionModule => module.Load(serviceCollection);
 
-        /// <summary>
-        /// Adds the provided <see cref="IServiceCollectionModule"/> to the <see cref="IServiceCollection"/>.
-        /// </summary>
-        /// <param name="serviceCollection">The service collection.</param>
-        /// <typeparam name="T">The type.</typeparam>
-        /// <returns>The service collection with dependencies registered.</returns>
-        public static IServiceCollection AddModule<T>(this IServiceCollection serviceCollection)
-            where T : IServiceCollectionModule, new() => new T().Load(serviceCollection);
-    }
+    /// <summary>
+    /// Adds the provided <see cref="IServiceCollectionModule"/> to the <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="serviceCollection">The service collection.</param>
+    /// <typeparam name="T">The type.</typeparam>
+    /// <returns>The service collection with dependencies registered.</returns>
+    public static IServiceCollection AddModule<T>(this IServiceCollection serviceCollection)
+        where T : IServiceCollectionModule, new() => new T().Load(serviceCollection);
 }
