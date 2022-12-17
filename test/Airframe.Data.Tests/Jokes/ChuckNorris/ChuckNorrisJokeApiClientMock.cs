a601ca4bdbf0943ce752e6ff07e94273650135c4
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-12-16 19:19:44 -0600

    fix up tests

--- test/Airframe.Data.Tests/Jokes/ChuckNorris/ChuckNorrisJokeApiClientMock.cs
+++ test/Airframe.Data.Tests/Jokes/ChuckNorris/ChuckNorrisJokeApiClientMock.cs
@@ -1,4 +1,4 @@
-using Rocket.Surgery.Airframe.Data.ChuckNorris;
+using Rocket.Surgery.Airframe.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,16 +12,28 @@ public class ChuckNorrisJokeApiClientMock : IChuckNorrisJokeApiClient
     private readonly IObservable<IEnumerable<ChuckNorrisJoke>> _jokeClient;
     private Subject<IEnumerable<ChuckNorrisJoke>> _jokes = new Subject<IEnumerable<ChuckNorrisJoke>>();
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChuckNorrisJokeApiClientMock"/> class.
+    /// </summary>
     public ChuckNorrisJokeApiClientMock() => _jokeClient = _jokes.AsObservable();
 
-    public IObservable<ChuckNorrisJoke> Random() => Observable.Create<ChuckNorrisJoke>(observer => _jokes.SelectMany(jokes => jokes).Subscribe(observer));
+    /// <inheritdoc/>
+    public IObservable<ChuckNorrisJoke> Random() =>
+        Observable.Create<ChuckNorrisJoke>(observer => _jokes.SelectMany(jokes => jokes).Subscribe(observer));
 
-    public IObservable<IEnumerable<ChuckNorrisJoke>> RandomFromCategory(string category) => Observable.Create<IEnumerable<ChuckNorrisJoke>>(observer => _jokes.Select(jokes => jokes.Where(x => x.Categories.Contains(category))).Subscribe(observer));
+    /// <inheritdoc/>
+    public IObservable<IEnumerable<ChuckNorrisJoke>> RandomFromCategory(string category) =>
+        Observable.Create<IEnumerable<ChuckNorrisJoke>>(observer =>
+            _jokes.Select(jokes =>
+                    jokes.Where(x => x.Categories.Contains(category)))
+               .Subscribe(observer));
 
+    /// <inheritdoc/>
     public IObservable<IEnumerable<string>> Categories() => null;
 
-    public IObservable<IEnumerable<ChuckNorrisJoke>> Search(string query)
-        => Observable.Create<IEnumerable<ChuckNorrisJoke>>(observer => _jokes.Subscribe(observer));
+    /// <inheritdoc/>
+    public IObservable<IEnumerable<ChuckNorrisJoke>> Search(string query) =>
+        Observable.Create<IEnumerable<ChuckNorrisJoke>>(observer => _jokes.Subscribe(observer));
 
     public void Notify(params ChuckNorrisJoke[] jokes) => _jokes.OnNext(jokes);
 }
