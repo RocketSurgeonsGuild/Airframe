e85e06298384f3a413cca4825c005e834d58bc11
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2024-02-09 02:26:07 -0600

    chore: cleaning

--- test/ViewModels.Tests/TestNavigationViewModelFixture.cs
+++ test/ViewModels.Tests/TestNavigationViewModelFixture.cs
@@ -1,12 +1,8 @@
-using Rocket.Surgery.Airframe.Navigation;
-
 namespace Rocket.Surgery.Airframe.ViewModels.Tests
 {
     internal class TestNavigationViewModelFixture
     {
         public static implicit operator TestNavigationViewModel(TestNavigationViewModelFixture fixture) => fixture.Build();
-
-        public INavigated AsNavigated() => Build();
 
         private TestNavigationViewModel Build() => new TestNavigationViewModel();
     }
