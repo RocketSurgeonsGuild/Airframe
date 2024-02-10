ad9adff31327a42ed1d8867a6ab2423fe0b87899
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2024-02-09 18:48:26 -0600

    chore: maccatalyst

--- .build/AirframeBuild.GitHubActions.cs
+++ .build/AirframeBuild.GitHubActions.cs
@@ -47,7 +47,7 @@ public partial class AirframeBuild
 
     public Target Workloads => definition => definition
        .Before(Restore)
-       .Executes(() => DotNetTasks.DotNetWorkloadInstall(configurator => configurator.SetWorkloadId("ios")));
+       .Executes(() => DotNetTasks.DotNetWorkloadInstall(configurator => configurator.SetWorkloadId("ios", "maccatalyst")));
 
     public static RocketSurgeonGitHubActionsConfiguration Middleware(RocketSurgeonGitHubActionsConfiguration configuration)
     {
