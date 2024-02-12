a61cd4add2603e0fe2c37b3f762d675418846c2e
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2024-02-11 21:39:35 -0600

    feature: getting these tests in order

--- test/Core.Tests/Listeners/ListenerTests.cs
+++ test/Core.Tests/Listeners/ListenerTests.cs
@@ -1,4 +1,6 @@
 using FluentAssertions;
+using System;
+using System.Reactive;
 using Xunit;
 
 namespace Rocket.Surgery.Airframe.Core.Tests.Listeners;
@@ -6,34 +8,49 @@ namespace Rocket.Surgery.Airframe.Core.Tests.Listeners;
 public class ListenerTests
 {
     [Fact]
-    public void GivenListening_When_ThenResultNotNull() =>
-
+    public void GivenListening_When_ThenResultNotNull()
+    {
         // Given
+        Unit? result = null;
+        TestListener sut = new TestListenerFixture();
+        using var _ = sut.As<IListener>().Listen().Subscribe(actual => result = actual);
+
         // When
+        sut.Notify(Unit.Default);
+
         // Then
-        true.Should().BeFalse();
+        result.Should().NotBeNull();
+    }
 
     [Fact]
-    public void GivenListening_WhenIgnore_ThenResultNull() =>
-
+    public void GivenListening_WhenIgnore_ThenResultNull()
+    {
         // Given
+        Unit? result = null;
+        TestListener sut = new TestListenerFixture();
+        using var _ = sut.As<IListener>().Listen().Subscribe(actual => result = actual);
+
         // When
+        using var __ = sut.As<IListener>().Ignore().Subscribe();
+        sut.Notify(Unit.Default);
+
         // Then
-        true.Should().BeFalse();
+        result.Should().BeNull();
+    }
 
     [Fact]
-    public void GivenIgnore_When_ThenResultNull() =>
-
+    public void GivenIgnore_WhenListen_ThenResultNotNull()
+    {
         // Given
-        // When
-        // Then
-        true.Should().BeFalse();
+        Unit? result = null;
+        TestListener sut = new TestListenerFixture();
+        using var _ = sut.As<IListener>().Ignore().Subscribe();
 
-    [Fact]
-    public void GivenIgnore_WhenListen_ThenResultNotNull() =>
-
-        // Given
         // When
+        using var __ = sut.As<IListener>().Listen().Subscribe(actual => result = actual);
+        sut.Notify(Unit.Default);
+
         // Then
-        true.Should().BeFalse();
+        result.Should().NotBeNull();
+    }
 }
---
+++ test/Core.Tests/Listeners/TestListener.cs
@@ -0,0 +1,22 @@
+using Airframe.Testing;
+using Microsoft.Extensions.Logging;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Rocket.Surgery.Airframe.Core.Tests.Listeners;
+
+internal class TestListener : Listener, INotifier<Unit>
+{
+    public TestListener(ILoggerFactory loggerFactory)
+        : base(loggerFactory)
+    {
+        _subject = new Subject<Unit>();
+        Listening = _subject.AsObservable().Publish();
+    }
+
+    /// <inheritdoc/>
+    public void Notify(Unit item) => _subject.OnNext(item);
+
+    private readonly ISubject<Unit> _subject;
+}
