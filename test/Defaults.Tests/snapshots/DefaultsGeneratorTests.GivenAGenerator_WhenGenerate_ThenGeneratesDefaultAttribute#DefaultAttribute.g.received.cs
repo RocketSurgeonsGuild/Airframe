b00c29de194eaec4033b6dfa25f2b1636d2aeb2d
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-02-22 09:50:55 -0600

    feature: add snapshot folder using the module initializer

---
+++ test/Defaults.Tests/snapshots/DefaultsGeneratorTests.GivenAGenerator_WhenGenerate_ThenGeneratesDefaultAttribute#DefaultAttribute.g.received.cs
@@ -0,0 +1,16 @@
+﻿//HintName: Rocket.Surgery.Airframe.Defaults/Rocket.Surgery.Airframe.Defaults.DefaultsGenerator/DefaultAttribute.g.cs
+#nullable enable
+using System;
+using System.Diagnostics;
+
+namespace Rocket.Surgery.Airframe.Defaults
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    [Conditional("CODEGEN")]
+    internal class DefaultAttribute : Attribute
+    {
+        public DefaultAttribute() => PropertyName = "Default";
+
+        public string PropertyName { get; set; }
+    }
+}
