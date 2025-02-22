e3922552c11e169d97f6d1b3a2cac68812b344a5
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-02-22 09:25:29 -0600

    : feature: add Airframe.Defaults

---
+++ src/Defaults/Defaults.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+
+namespace Rocket.Surgery.Airframe.Defaults;
+
+internal static class Defaults
+{
+    public static string Version { get; } =
+        typeof(DefaultsGenerator).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+     ?? typeof(DefaultsGenerator).Assembly.GetName().Version.ToString();
+
+    public static string CodeGenerationAttribute { get; } = $@"[System.CodeDom.Compiler.GeneratedCode(""Rocket.Surgery.Airframe.Defaults"", ""{Version}"")]";
+}
