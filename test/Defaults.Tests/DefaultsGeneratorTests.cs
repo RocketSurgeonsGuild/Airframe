61540bfe80fe543fb6acee70473089a86b87848c
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-02-22 09:25:29 -0600

    : feature: add Airframe.Defaults

---
+++ test/Defaults.Tests/DefaultsGeneratorTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Logging;
+using Rocket.Surgery.Extensions.Testing.SourceGenerators;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VerifyXunit;
+
+namespace Rocket.Surgery.Airframe.Defaults.Tests;
+
+public class DefaultsGeneratorTests
+{
+    [Fact]
+    public async Task Given_When_Then()
+    {
+        // Given, When
+        var result = await GeneratorTestContextBuilder
+           .Create()
+           .WithGenerator<DefaultsGenerator>()
+           .AddReferences(typeof(ILogger<>))
+           .AddReferences(typeof(List<>))
+           .Build()
+           .GenerateAsync();
+
+        // Then
+        await Verifier.Verify(result).ScrubLines(text => text.Contains("System.CodeDom.Compiler.GeneratedCode"));
+    }
+}
