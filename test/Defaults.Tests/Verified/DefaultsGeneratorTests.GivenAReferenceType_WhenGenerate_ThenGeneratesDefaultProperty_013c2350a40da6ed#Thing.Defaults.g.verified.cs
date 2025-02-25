aa66d19caaae8347731426bf2bd5dea312445c81
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-02-24 21:49:24 -0600

    feature: add failing tests for object construction

---
+++ test/Defaults.Tests/Verified/DefaultsGeneratorTests.GivenAReferenceType_WhenGenerate_ThenGeneratesDefaultProperty_013c2350a40da6ed#Thing.Defaults.g.verified.cs
@@ -0,0 +1,8 @@
+//HintName: Rocket.Surgery.Airframe.Defaults/Rocket.Surgery.Airframe.Defaults.DefaultsGenerator/Thing.Defaults.g.cs
+namespace Rocket.Surgery.Airframe.Defaults
+{
+    public partial class Thing
+    {
+        public static Thing Default { get; } = new Thing(default);
+    }
+}
