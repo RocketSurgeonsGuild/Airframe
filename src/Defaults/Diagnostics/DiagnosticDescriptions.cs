17464943ee7b583422b93f6b391ce520fb0ef7ef
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-03-12 23:54:29 -0500

    feature: add RSAD0002

--- src/Defaults/Diagnostics/DiagnosticDescriptions.cs
+++ src/Defaults/Diagnostics/DiagnosticDescriptions.cs
@@ -9,7 +9,18 @@ internal static class DiagnosticDescriptions
     /// </summary>
     public static DiagnosticDescriptor Rsad0001 { get; } = new(
         nameof(Rsad0001).ToUpperInvariant(),
-        "Inaccessible constructor.",
+        "Inaccessible constructor",
+        string.Empty,
+        "Support",
+        DiagnosticSeverity.Error,
+        true);
+
+    /// <summary>
+    ///    Gets a diagnostic for unsupported classes with multiple constructors.
+    /// </summary>
+    public static DiagnosticDescriptor Rsad0002 { get; } = new(
+        nameof(Rsad0002).ToUpperInvariant(),
+        "Multiple constructors",
         string.Empty,
         "Support",
         DiagnosticSeverity.Error,
