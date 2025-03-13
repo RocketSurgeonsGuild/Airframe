f86fbf3d57405046b616e7cf24d511437c762d92
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2025-03-12 21:12:46 -0500

    chore: taxonomy of the concerns (#280)

---
+++ test/Defaults.Tests/Generator/DefaultsGeneratorTests.cs
@@ -0,0 +1,50 @@
+using Rocket.Surgery.Airframe.Defaults.Generator;
+using Rocket.Surgery.Airframe.Defaults.Tests.Data;
+using Rocket.Surgery.Extensions.Testing.SourceGenerators;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VerifyXunit;
+
+namespace Rocket.Surgery.Airframe.Defaults.Tests.Generator;
+
+public class DefaultsGeneratorTests
+{
+    [Fact]
+    public async Task GivenAGenerator_WhenGenerate_ThenAttributeGenerated()
+    {
+        // Given, When
+        var result = await GeneratorTestContextBuilder
+           .Create()
+           .WithGenerator<DefaultsGenerator>()
+           .AddReferences(typeof(List<>))
+           .Build()
+           .GenerateAsync();
+
+        // Then
+        await Verifier.Verify(result).ScrubLines(text => text.Contains("System.CodeDom.Compiler.GeneratedCode"));
+    }
+
+    [Theory]
+    [MemberData(nameof(SimpleReferenceTypeData.Data), MemberType = typeof(SimpleReferenceTypeData))]
+    [MemberData(nameof(PrimitivePropertyReferenceTypeData.Data), MemberType = typeof(PrimitivePropertyReferenceTypeData))]
+    [MemberData(nameof(ReferencePropertyData.Data), MemberType = typeof(ReferencePropertyData))]
+    public async Task GivenAReferenceType_WhenGenerate_ThenGeneratesDefaultProperty(GeneratorTestContext context)
+    {
+        // Given, When
+        var result = await context.GenerateAsync();
+
+        // Then
+        await Verifier.Verify(result).HashParameters().UseParameters(context.Id);
+    }
+
+    [Theory]
+    [MemberData(nameof(NoAttributeData.Data), MemberType = typeof(NoAttributeData))]
+    public async Task GivenAReferenceType_WhenGenerate_ThenDoesNotGenerateDefaultProperty(GeneratorTestContext context)
+    {
+        // Given, When
+        var result = await context.GenerateAsync();
+
+        // Then
+        await Verifier.Verify(result).HashParameters().UseParameters(context.Id);
+    }
+}
--- test/Defaults.Tests/Verified/DefaultsGeneratorTests.Iterating_8463fc0cb3de0c8f#Thing.Defaults.g.verified.cs
+++
@@ -1,8 +0,0 @@
-//HintName: Rocket.Surgery.Airframe.Defaults/Rocket.Surgery.Airframe.Defaults.DefaultsGenerator/Thing.Defaults.g.cs
-namespace Rocket.Surgery.Airframe.Defaults
-{
-    public partial class Thing
-    {
-        public static Thing Default { get; } = new Thing(Stuff.Default);
-    }
-}
