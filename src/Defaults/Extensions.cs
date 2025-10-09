28a0e669ebeba0a79996df723c600b433f1a2860
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-10-08 21:26:09 -0500

    reverting everything because Matthias recommended Scriban

--- src/Defaults/Extensions.cs
+++ src/Defaults/Extensions.cs
@@ -8,16 +8,11 @@ namespace Rocket.Surgery.Airframe.Defaults;
 
 internal static class Extensions
 {
-    public static SyntaxList<MemberDeclarationSyntax> GeneratePartialClassWithProperty(
-        this INamedTypeSymbol namedTypeSymbol,
-        IdentifierNameSyntax className,
-        AttributeData attribute,
-        ArgumentListSyntax? argumentListSyntax)
     public static bool HasAccessibility(this IMethodSymbol symbol, params Accessibility[] accessibility)
         => accessibility.Contains(symbol.DeclaredAccessibility);
 
     public static bool HasPublicAccess(this ConstructorDeclarationSyntax syntax) => syntax.Modifiers.Any(token => token.IsKind(SyntaxKind.PublicKeyword));
 
-    public static bool IsDefaultsAttribute(this AttributeSyntax syntax) =>
-        ((IdentifierNameSyntax)syntax.Name).Identifier.ValueText is nameof(DefaultsAttribute) or "Defaults";
+    public static bool IsDefaultsAttribute(this AttributeSyntax syntax)
+        => ((IdentifierNameSyntax)syntax.Name).Identifier.ValueText is nameof(DefaultsAttribute) or "Defaults";
 }
