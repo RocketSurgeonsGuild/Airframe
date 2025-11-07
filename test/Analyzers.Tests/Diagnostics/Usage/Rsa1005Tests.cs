9dd42a0e1d725f31e6f968fbc1f587689f6b17a9
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-11-07 08:55:51 -0600

    add test

---
+++ test/Analyzers.Tests/Diagnostics/Usage/Rsa1005Tests.cs
@@ -0,0 +1,79 @@
+using FluentAssertions;
+using Rocket.Surgery.Airframe.Analyzers.Diagnostics.Usage;
+using Rocket.Surgery.Extensions.Testing.SourceGenerators;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using VerifyXunit;
+
+namespace Rocket.Surgery.Airframe.Analyzers.Tests.Diagnostics.Usage;
+
+public class Rsa1005Tests
+{
+    [Theory]
+    [InlineData(Rsa1005TestData.Incorrect)]
+    public async Task GivenIncorrect_WhenAnalyze_ThenDiagnosticsReported(string source)
+    {
+        // Given. When
+        var result = await GeneratorTestContextBuilder
+           .Create()
+           .AddSources(source)
+           .WithAnalyzer<Rsa1005>()
+           .GenerateAsync();
+
+        // Then
+        result
+           .AnalyzerResults
+           .Should()
+           .Contain(pair => pair.Value.Diagnostics.All(diagnostic => diagnostic.Id == Descriptions.RSA1005.Id));
+    }
+
+    [Theory]
+    [InlineData(nameof(Rsa1005TestData.Incorrect), Rsa1005TestData.Incorrect)]
+    public async Task GivenSource_WhenAnalyze_ThenVerify(string name, string source)
+    {
+        // Given, When
+        var result = await GeneratorTestContextBuilder
+           .Create()
+           .WithAnalyzer<Rsa1005>()
+           .AddSources(source)
+           .AddReferences(typeof(Unit), typeof(Expression<>))
+           .GenerateAsync();
+
+        // Then
+        await Verifier.Verify(result).HashParameters().UseParameters(name).DisableRequireUniquePrefix();
+    }
+
+    private static class Rsa1005TestData
+    {
+        // lang=csharp
+        public const string Incorrect = """
+            using System;
+            using System.Reactive;
+            using System.Reactive.Concurrency;
+            using System.Reactive.Linq;
+
+            namespace Foo.Bar;
+
+            public class Rsa1005Example
+            {
+                public Rsa1005Example() =>
+                    Observable
+                       .Return(Unit.Default)
+                       .Throttle(TimeSpan.Zero)
+                       .Subscribe();
+            }
+            """;
+    }
+}
+
+public class Rsa1005Example
+{
+    public Rsa1005Example() => Observable
+       .Return(Unit.Default)
+       .Throttle(TimeSpan.Zero)
+       .Subscribe();
+}
