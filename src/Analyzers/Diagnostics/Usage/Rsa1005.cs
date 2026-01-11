70dea76f204cd0f5df5a7033610907ef9a9c69c4
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2026-01-10 23:20:20 -0500

    feature: RSA1005 (#342)
    * feature: RSA1005
    
    add some refactoring
    
    add initial generated code
    
    * add code fix
    
    * add test
    
    * make test pass
    
    clean up warnings
    
    * downgrade to info after realizing observable return has this problem

---
+++ src/Analyzers/Diagnostics/Usage/Rsa1005.cs
@@ -0,0 +1,107 @@
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using static Rocket.Surgery.Airframe.Analyzers.Descriptions;
+
+namespace Rocket.Surgery.Airframe.Analyzers.Diagnostics.Usage;
+
+/// <summary>
+/// Represents a diagnostic for <see cref="Descriptions.RSA1005"/>.
+/// </summary>
+public class Rsa1005 : Rsa1000
+{
+    /// <inheritdoc/>
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = [RSA1005];
+
+    /// <inheritdoc/>
+    protected override void Analyze(SyntaxNodeAnalysisContext context) => AnalyzeInvocation(context);
+
+    private static void AnalyzeInvocation(SyntaxNodeAnalysisContext context)
+    {
+        var invocation = (InvocationExpressionSyntax)context.Node;
+        var symbolInfo = context.SemanticModel.GetSymbolInfo(invocation);
+
+        if (symbolInfo.Symbol is not IMethodSymbol method)
+        {
+            return;
+        }
+
+        // Handle extension methods
+        var actualMethod = method.ReducedFrom ?? method;
+
+        // Check if the method is from System.Reactive namespace
+        if (!IsReactiveExtensionsMethod(actualMethod))
+        {
+            return;
+        }
+
+        // Check if this method already has an IScheduler parameter
+        if (HasSchedulerParameter(actualMethod))
+        {
+            return;
+        }
+
+        // Check if there's an overload with IScheduler available
+        if (!HasSchedulerOverload(actualMethod))
+        {
+            return;
+        }
+
+        var diagnostic = Diagnostic.Create(RSA1005, GetMethodNameLocation(invocation), actualMethod.Name);
+
+        context.ReportDiagnostic(diagnostic);
+    }
+
+    private static bool IsReactiveExtensionsMethod(IMethodSymbol method)
+    {
+        // Check if the method is from System.Reactive namespaces
+        var containingNamespace = method.ContainingNamespace?.ToDisplayString();
+        return containingNamespace != null &&
+            (containingNamespace.StartsWith("System.Reactive") ||
+                containingNamespace.StartsWith("System.Observable"));
+    }
+
+    private static bool HasSchedulerParameter(IMethodSymbol method) => method.Parameters.Any(parameterSymbol => IsSchedulerType(parameterSymbol.Type));
+
+    private static bool HasSchedulerOverload(IMethodSymbol method)
+    {
+        // Get all methods with the same name in the same type
+        var allMethods = method.ContainingType.GetMembers(method.Name)
+           .OfType<IMethodSymbol>()
+           .Where(methodSymbol => methodSymbol.IsStatic == method.IsStatic);
+
+        // Check if any overload has a scheduler parameter
+        return allMethods.Any(m => HasSchedulerParameter(m));
+    }
+
+    private static bool IsSchedulerType(ITypeSymbol? type)
+    {
+        if (type == null)
+        {
+            return false;
+        }
+
+        // Check for IScheduler interface
+        if (type.Name == "IScheduler" &&
+            type.ContainingNamespace?.ToDisplayString() == "System.Reactive.Concurrency")
+        {
+            return true;
+        }
+
+        // Check implemented interfaces
+        return type.AllInterfaces.Any(i =>
+            i.Name == "IScheduler" &&
+            i.ContainingNamespace?.ToDisplayString() == "System.Reactive.Concurrency");
+    }
+
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1025:Code should not contain multiple whitespace in a row", Justification = "Looks better.")]
+    private static Location GetMethodNameLocation(InvocationExpressionSyntax invocation) => invocation.Expression switch
+    {
+        MemberAccessExpressionSyntax memberAccess => memberAccess.Name.GetLocation(),
+        IdentifierNameSyntax identifier           => identifier.GetLocation(),
+        var _                                     => invocation.GetLocation(),
+    };
+}
